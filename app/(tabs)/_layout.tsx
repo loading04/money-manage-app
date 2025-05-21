d6f92e82cfa6aac77443153536bfb1ea9658a11d
Ahmed Aziz Smaoui <42848675+loading04@users.noreply.github.com>
2025-05-21 23:33:17 +0000

    add lucide package | icons nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,47 @@
 
 import { Tabs } from 'expo-router'
-
+import { ChartColumnIncreasing, House, Settings, Wallet } from 'lucide-react-native'
 export default function TabLayout(){
 
     return (
-      <Tabs screenOptions={{headerShown:false
+      <Tabs 
+      screenOptions={{
+        headerShown:false,
+        tabBarShowLabel:false,
+        
 
       }}>
         <Tabs.Screen 
         name='index'
-        options={{headerShown:false}}
+        options={{
+          headerShown:false,
+          tabBarIcon:()=> <House />,
+        }}
         />
-        <Tabs.Screen 
-        name='budgets'
-        options={{headerShown:false}}
-        />
+
         <Tabs.Screen 
         name='transactions'
-        options={{headerShown:false}}
+        options={{
+          headerShown:false,
+          tabBarIcon:()=> <Wallet  />,
+
+        }}
         />
+
+        <Tabs.Screen 
+        name='budgets'
+        options={{
+          headerShown:false,
+          tabBarIcon:()=> <ChartColumnIncreasing   />,
+        }}
+        />
+
         <Tabs.Screen 
         name='settings'
-        options={{headerShown:false}}
+        options={{
+          headerShown:false,
+          tabBarIcon:()=> <Settings   />,
+        }}
         />
       </Tabs>
     )
