ee5ecd94b92780d7e747182dab06d26a409d1005
Ahmed Aziz Smaoui <42848675+loading04@users.noreply.github.com>
2025-06-03 17:29:29 +0000

    dashboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,21 @@
 import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { HStack } from "@/components/ui/hstack";
-import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
-import { TrendingUp } from "lucide-react-native";
-import { View } from "react-native";
+import { PiggyBank, TrendingDown, TrendingUp, Wallet } from "lucide-react-native";
+import { Text, View } from "react-native";
 import { COLORS } from '../constants/theme';
-
 export default function Index() {
   const totalBalance = 4750.75;
-
   return (
     <View className="bg-white-300 mt-4">
       {/* Total Balance Card */}
       <Card size="md" variant="outline" className="mb-4 m-4">
-        <Heading size="md" className="mb-1" bold>
+        <Heading size="md" className="mb-1 pt-4 pl-4 text-2xl font-semibold" >
           Total Balance
         </Heading>
-        <Text bold className="font-bold" size="2xl">
-          {totalBalance}
+        <Text className="font-bold color-gray-950 text-4xl pl-4"  >
+          ${totalBalance}
         </Text>
       </Card>
 
@@ -41,33 +38,112 @@ export default function Index() {
                 Income
               </Heading>
 
-              <Text bold size="lg" >
-                3,200.00
+              <Text className="text-xl color-black font-semibold" >
+                $3,200.00
               </Text>
             </VStack>
-
           </HStack>
-
-
-
         </Card>
 
         {/* Expenses Card */}
         <Card
           size="md"
           variant="outline"
-          className="flex-1 ml-2 m-4"
+          className="flex-1 mr-2 m-4"
         >
 
-          <Heading size="sm" className="mb-1 dark:text-stone-500">
-            Expenses
-          </Heading>
-          <Text bold size="lg" >
-            1,850.25
-          </Text>
+          <HStack space="4xl">
+            <View className="bg-red-200 rounded-full w-10 h-10 items-center justify-center mt-3----------------------" >
+              <TrendingDown className="h-5 w-5" color={COLORS.red} />
+            </View>
 
+            <VStack>
+              <Heading size="sm" className="mb-1 dark:text-stone-500">
+                Expenses
+              </Heading>
+
+              <Text className="text-xl color-black font-semibold" >
+                $1,200.00
+              </Text>
+            </VStack>
+          </HStack>
         </Card>
       </HStack>
+
+      <HStack space="lg">
+        {/* Income Card */}
+        <Card
+          size="md"
+          variant="outline"
+          className="flex-1 mr-2 m-4"
+        >
+
+          <HStack space="4xl">
+            <View className="bg-blue-200 rounded-full w-10 h-10 items-center justify-center mt-3----------------------" >
+              <Wallet className="h-5 w-5" color={COLORS.blue} />
+            </View>
+
+            <VStack>
+              <Heading size="sm" className="mb-1 dark:text-stone-500">
+                Monthly Budget
+              </Heading>
+
+              <Text className="text-xl color-black font-semibold" >
+                $3,200.00
+              </Text>
+            </VStack>
+          </HStack>
+        </Card>
+
+        {/* Expenses Card */}
+        <Card
+          size="md"
+          variant="outline"
+          className="flex-1 mr-2 m-4"
+        >
+
+          <HStack space="4xl">
+            <View className="bg-purple-200 rounded-full w-10 h-10 items-center justify-center mt-3----------------------" >
+              <PiggyBank className="h-5 w-5" color={COLORS.purple} />
+            </View>
+
+            <VStack>
+              <Heading size="sm" className="mb-1 dark:text-stone-500">
+                Saving
+              </Heading>
+              <Text className="text-xl color-black font-semibold" >
+                $1,200.00
+              </Text>
+            </VStack>
+          </HStack>
+        </Card>
+      </HStack>
+
+
+
+      <Card size="md" variant="outline" className="mb-4 m-4">
+        <Heading size="md" className="mb-1 pt-4 pl-4 text-2xl font-semibold" >
+          Budget Progress
+        </Heading>
+
+        <HStack className="pl-4 pt-4">
+          <Text className="text-xl font-semibold">
+           Housing
+          </Text>
+          <Text className="absolute right-0 text-lg">
+            $1,150.00 / $1,200.00
+          </Text>
+          
+        </HStack>
+
+        <View className="h-2  bg-gray-200 rounded-full overflow-hidden ml-4 mt-2">
+          <View className="h-full bg-green-500 rounded-full  " style={{ width: '96%' }} />
+        </View>
+          
+       
+
+      </Card>
+
     </View>
   );
 }
--- app/constants/theme.tsx
+++ app/constants/theme.tsx
@@ -1,5 +1,9 @@
 export const COLORS = {
     clicked: "#2596be",
     not_clickt: "#873e23",
-    green: "#008000"
+    green: "#008000",
+    red: "#E53E6E",
+    red_light: "#FEE2E2",
+    blue: "#3186CE",
+    purple: "#805AD5",
 } as const;
